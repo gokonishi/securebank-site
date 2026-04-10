d3f4dbddede05b5487e2ca97165639306d04f2fb
Go Konishi <gokonishi@gmail.com>
2026-04-11 00:08:04 +0900

    Move redirect to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   images: {
     unoptimized: false,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "securebank.jp" }],
+        destination: "https://securebank.co.jp/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
