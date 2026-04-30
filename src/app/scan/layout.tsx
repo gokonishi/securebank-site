c3037cf1fd18021fce164246a6f1cae65f2673b9
Go Konishi <gokonishi@gmail.com>
2026-04-30 23:26:30 +0900

    Add scan layout to override global styles

---
+++ src/app/scan/layout.tsx
@@ -0,0 +1,9 @@
+﻿export default function ScanLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body style={{ margin: 0, padding: 0, background: "#080808", color: "#fff" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
